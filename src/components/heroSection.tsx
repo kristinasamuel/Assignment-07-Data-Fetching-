791e68c543d8ac6103f9eff1d50a0723e636b19e
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-12-21 21:13:42 +0500

    Assignment Fetching data completed

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -3,11 +3,11 @@
 export default function HeroSection() {
   return (
     <div className="w-full bg-gray-400 min-h-screen flex flex-col justify-center p-4">
-    <h3 className="font-bold text-[36px] sm:text-[48px] text-center">
+    <h3 className="font-bold text-[36px] sm:text-[48px] text-center mt-10">
       Exploring Data Fetching
     </h3>
     <p className="font-medium text-[22px] sm:text-[24px] text-center mt-4">
-      Data fetching refers to the process of getting data from an external source (like an API or database)..
+      Data fetching refers to the process of getting data from an external source (like an API or database).
     </p>
     <div className="flex flex-col lg:flex-row justify-center items-center mt-10 space-y-5 lg:space-y-0 lg:space-x-8">
       <div className="p-4 max-w-xl w-full sm:w-auto">
